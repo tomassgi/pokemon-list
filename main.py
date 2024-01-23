2222f3389e692b7138053bcfd4e3883ec06a1bba
Ilya Karpovs <karpov.ilja@gmail.com>
2024-01-23 23:46:54 +0200

    Task and file

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
 
+import csv
+
+pokemons = []
+
+with open('pokemon.csv', newline='') as csv_file:
+    file_reader = csv.reader(csv_file, delimiter = ',', quotechar='|')
+
+    for row in file_reader:
+        pokemons.append(row[0])
+
+# print(pokemons)
+
+print("Pokemon list command:")
+
+while True:
+    print("1. Get pokemon by sequence number")
+    print("2. Sort by A-Z")
+    print("3. Sort by Z-A")
+    print("4. Search by name")
+    print("5. Search by length of name")
+    print("6. Exit")
+
+    choice = input("Enter your choice (1-6): ")
+
+    if choice == '1':
+        # https://www.w3schools.com/python/python_lists_access.asp
+        pass
+    elif choice == '2':
+        # https://www.w3schools.com/python/python_lists_sort.asp
+        pass
+    elif choice == '3':
+        # https://www.w3schools.com/python/python_lists_sort.asp
+        pass
+    elif choice == '4':
+        # https://www.w3schools.com/python/python_lists_comprehension.asp
+        # https://www.w3schools.com/python/ref_string_startswith.asp
+        pass
+    elif choice == '5':
+        # https://www.w3schools.com/python/python_lists_comprehension.asp
+        pass
+    elif choice == '6':
+        print("Exiting")
+        break
+    else:
+        print("Invalid choice, choose from 1 to 6")
+
+    print("==========================")
